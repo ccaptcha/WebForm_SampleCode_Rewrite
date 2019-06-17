518b4e15ed58050451f351a3d690631675e2641c
ccaptcha <51909591+ccaptcha@users.noreply.github.com>
2019-06-17 19:40:44 +0430

    Webform_sampleCode_CCAPTCHA_REWrite

---
+++ SampleCCaptchaASP.Net/SampleCCaptcha.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SampleCCaptcha.aspx.cs" Inherits="SampleCCaptchaASP.Net.SampleCCaptcha" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<script src="https://widget.ccaptcha.com/js/ccaptcha_version2_M1.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div  id="Ccaptcha_M1" data-ccaptcha_apicode="YOUR DOMAIN API CODE"></div>
+
+
+        <br />
+        <asp:Button Text="Confirm" runat="server"  OnClick="Confirm_Click" /> 
+        <br />
+        <asp:Label Text="" ID="lbl" runat="server" />
+    </form>
+</body>
+</html>
---
+++ SampleCCaptchaASP.Net/SampleCCaptcha.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleCCaptchaASP.Net
+{
+    public partial class SampleCCaptcha : System.Web.UI.Page
+    {
+        public class Info
+        {
+            public string Token { get; set; }
+            public string Secret_Code { get; set; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Confirm_Click(object sender, EventArgs e)
+        {
+            Info info = new Info();
+            info.Secret_Code = "YOUR SECRET CODE";
+            if (Request.Form["ccaptcha_token_input"] != null)
+                info.Token = (Request.Form["ccaptcha_token_input"]).ToString();
+
+            // connect  to  webservice
+            WebClient objclient = new WebClient();
+            objclient.Headers[HttpRequestHeader.ContentType] = "application/json ; charset=utf-8";
+            objclient.Encoding = UTF8Encoding.UTF8;
+
+            //for use JsonConvert Serializer please install Newtonsoft package
+            string json = JsonConvert.SerializeObject(info);
+            string Response = objclient.UploadString("https://api.ccaptcha.com/api/Validate/ValidationPost", json);
+
+            if (Response == "\"true\"")
+            {
+                lbl.Text = "true"; // Correctly solved
+            }
+            else
+            {
+                lbl.Text = "false";
+            }
+
+        }
+    }
+}
